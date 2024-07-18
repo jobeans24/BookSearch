fe9e5f38d9db0988fee2c20a0c3ae19b3b1bb99f
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-07-18 12:41:04 -0400

    creating the build and getting all import files added. As well as creating the mutations.js and the queries.js files and writing out the code to allow appolo to create queries and organize the application data

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -9,7 +9,45 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+const jwt = require('jsonwebtoken');
 
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function (req, res, next) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: 'You have no token!' });
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
+    }
+
+    // send to next endpoint
+    next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
